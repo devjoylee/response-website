d2fd4756adfc17f0835159ecdd8ff32b02ef6bae
Joy Lee <joylee.dev@gmail.com>
2024-04-26 10:55:59 -0700

    feat: add routers to each pages

---
+++ app/service/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+const ServicePage = () => {
+  return <div className={styles.pageContainer}>ServicePage</div>;
+};
+
+export default ServicePage;
---
+++ app/work/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+const WorkPage = () => {
+  return <div className={styles.pageContainer}>WorkPage</div>;
+};
+
+export default WorkPage;
