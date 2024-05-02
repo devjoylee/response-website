97f37135f63374cc519facfed90160b99f1d5cf2
Joy Lee <joylee.dev@gmail.com>
2024-05-02 12:21:53 -0700

    feat: create a slider on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import { Headline } from './components';
 import styles from './styles.module.css';
+import { workList } from './work/list';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className={`${styles.homePage} maxWidth`}>
+    <div className={`${styles.homePage}`}>
       <Headline
         width={260}
         first='The return on'
@@ -19,7 +22,20 @@ export default function Home() {
         secondR='agency'
       />
 
-      <h3 className='neue-b'>Our work</h3>
+      <section className={styles.works}>
+        <h3 className='neue-b maxWidth'>Our work</h3>
+        <div className={styles.slider}>
+          {workList.slice(0, 5).map((item, i) => (
+            <Link href='/work' key={i}>
+              <Image src={item.imageURL} alt={item.title} width={500} height={0} />
+              <div className={styles.overlay}>
+                <span>{item.client.toUpperCase()}</span>
+                <p>{item.title}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
