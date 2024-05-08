d6d952e27a78f9aea0c15b956a2a696bad3b433f
Joy Lee <joylee.dev@gmail.com>
2024-05-07 22:16:23 -0400

    feat: add mobile navigation menu

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles.module.css';
-import { Navbar, SocialIcons } from '@/app/components';
+import { Navbar } from '@/app/components';
 
 interface HeaderProps {
   isMain?: boolean;
@@ -22,8 +22,7 @@ const Header = ({ isMain }: HeaderProps) => {
           />
         </Link>
 
-        <Navbar />
-        <SocialIcons whiteBG={isMain} />
+        <Navbar isMain={isMain} />
       </div>
     </header>
   );
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from './styles.module.css';
+import SocialIcons from '../SocialIcons/SocialIcons';
 import { usePathname } from 'next/navigation';
+import FadeMotion from '../FadeMotion/FadeMotion';
 
 const navs = [
   { id: 0, name: 'About', url: '/about' },
@@ -11,17 +13,35 @@ const navs = [
   { id: 2, name: 'Work', url: '/work' },
 ];
 
-const Navbar = () => {
+const Navbar = ({ isMain }: { isMain?: boolean }) => {
   const pathname = usePathname();
+  const [isOpened, setIsOpened] = useState(false);
+  const toggleNavbar = () => setIsOpened((o) => !o);
 
   return (
-    <ul className={`${styles.nav} neue-r`}>
-      {navs.map((nav) => (
-        <li key={nav.id} className={pathname === nav.url ? styles.active : ''}>
-          <Link href={nav.url}>{nav.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <div className={styles.nav}>
+      <div className={`${styles.navInner} ${isOpened ? styles.open : ''}`} onClick={toggleNavbar}>
+        <ul className={`${styles.navlist} neue-r`}>
+          {navs.map((nav) => (
+            <li key={nav.id} className={pathname === nav.url ? styles.active : ''}>
+              <Link href={nav.url}>{nav.name}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <SocialIcons whiteBG={isMain} />
+      </div>
+
+      <button
+        className={`${styles.navbtn} ${isOpened ? styles.clicked : ''}`}
+        onClick={toggleNavbar}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
   );
 };
 
